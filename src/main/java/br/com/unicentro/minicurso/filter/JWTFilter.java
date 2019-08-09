cd131a71894e6200a77d9f08f5dd9c548ff99d94
Samuel Alles Remlinger <samu_65@hotmail.com>
2019-08-08 21:49:41 -0300

    autenticação funcionando

---
+++ src/main/java/br/com/unicentro/minicurso/filter/JWTFilter.java
@@ -0,0 +1,40 @@
+package br.com.unicentro.minicurso.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+
+public class JWTFilter extends GenericFilterBean {
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;	
+		
+		if (!"OPTIONS".equals(request.getMethod()) && !request.getServletPath().startsWith("/auth/login")) {           
+			String token = request.getHeader("Authorization");
+			
+			try {
+				Jwts.parser().setSigningKey("SUA_CHAVE_SECREATA").parseClaimsJws(token).getBody(); 
+				chain.doFilter(req, res);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+		} else {
+			chain.doFilter(req, res);
+		}	
+	}
+
+}
+
