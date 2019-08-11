c97f7d07afac725571d1457e680f9fc41ec21eee
Samuel Alles Remlinger <samu_65@hotmail.com>
2019-08-11 08:26:27 -0300

    adicionados exemplos de query com string

--- src/main/java/br/com/unicentro/minicurso/repository/AlunoRepository.java
+++ src/main/java/br/com/unicentro/minicurso/repository/AlunoRepository.java
@@ -3,6 +3,7 @@ package br.com.unicentro.minicurso.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.unicentro.minicurso.model.Aluno;
@@ -15,5 +16,11 @@ public interface AlunoRepository extends PagingAndSortingRepository<Aluno, Long>
 	public List<Aluno> findByNomeContainingAndPeriodo(String nome, Integer periodo);
 	public List<Aluno> findByCurso(Curso curso);
 	public List<Aluno> findByCurso_NomeContaining(String cursoNome);
+	
+	@Query("select a from Aluno a")
+	public List<Aluno> metodoComJPQL();
+	
+	@Query(value = "select * from aluno", nativeQuery = true)
+	public List<Aluno> metodoComSQL();
 	
 }
